08764673b780519d82f1028915030f5c905499b1
Arpit Vaishnav <arpit.vaishnav@clariontechnologies.co.in>
2016-06-10 16:36:14 +0530

    updated initializer

--- lib/dragonfly-activerecord/store.rb
+++ lib/dragonfly-activerecord/store.rb
@@ -5,6 +5,10 @@ require 'active_record'
 module Dragonfly::ActiveRecord
   class Store
 
+    def initialize({})
+
+    end
+    
     # +temp_object+ should respond to +data+ and +meta+
     def write(temp_object, opts={})
       temp_object.file do |fd|
